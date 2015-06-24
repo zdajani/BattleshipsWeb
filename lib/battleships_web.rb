30c7120202d1c26e39290194c54b0e9a8d6038f3
Gerard Morera <grdmorera@gmail.com>
2015-06-24 12:18:46 +0100

    starting new game feature test extended

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,13 +1,21 @@
+require 'battleships'
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
-  set :views, proc { File.join(root, '..', 'views') }
 
+  set :views, proc { File.join(root, '..', 'views') }
+  run! if app_file == $0
   
   get '/' do
     'Hello BattleshipsWeb!'
   end
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+  get '/new_game' do 
+    $game  = Game.new Player, Board
+
+    @board = $game.own_board_view $game.player_1
+
+    erb :new_game
+  end
+
 end
--- spec/features/starting_new_game_spec.rb
+++ spec/features/starting_new_game_spec.rb
@@ -2,7 +2,18 @@ require 'spec_helper'
 
 feature 'Starting a new game' do
   scenario 'setting up ships on a board' do
+    
     visit '/new_game'
-    expect(page).to have_selector('board')
+    
+    expect(page).to have_selector '.board'
+    
+    expect(page).to have_content 'submarine'
+    expect(page).to have_content 'coords'
+    expect(page).to have_content 'direction'
+    fill_in('coords', :with => 'A1')
+    fill_in('direction', :with => 'Hortizonal')
+    click_on 'Place Ship'
+
   end
+  
 end
