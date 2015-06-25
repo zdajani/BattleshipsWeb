52fa8ce4c87b5a6ca1f611af9c03a9de55161278
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-25 16:48:22 +0100

    created a two player game

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -6,19 +6,26 @@ class BattleshipsWeb < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
   run! if app_file == $0
 
+  PLAYERS = ['player_2', 'player_1']
+
   get '/' do
     name = params[:name]
-    # session[:session]= name
     erb :index
   end
 
-  get '/new_game' do
-    $game  = Game.new Player, Board
-    @board = $game.own_board_view $game.player_1
+
+  post '/new_game' do
+      $game ||= Game.new Player, Board
+       assign_player unless PLAYERS.empty?
+
+
+    # $game  ||= Game.new Player, Board
+
+    @board = $game.own_board_view $game.send(session[:player])
     erb :new_game
   end
 
-  post '/new_game' do
+  post '/placing_ships' do
 
     ships_hash = {'battleship'          => Ship.battleship,
                   'submarine'           => Ship.submarine,
@@ -26,31 +33,47 @@ class BattleshipsWeb < Sinatra::Base
                   'aircraft_carrier'    => Ship.aircraft_carrier,
                   'destroyer'           => Ship.destroyer}
     begin
-    $game.player_1.place_ship ships_hash[params[:shiptypes]], params[:coords], params[:orientation]
+    $game.send(session[:player]).place_ship ships_hash[params[:shiptypes]], params[:coords], params[:orientation]
     rescue
     @error = "Invalid Coordinate Try Again!"
     end
-    @board = $game.own_board_view $game.player_1
+    @board = $game.own_board_view $game.send(session[:player])
 
     erb :new_game
   end
 
-  get '/player1_game' do
-    @board  = $game.own_board_view $game.player_1
-    @board2 = $game.opponent_board_view $game.player_1
+  get '/stage_two' do
+    @board  = $game.own_board_view $game.send(session[:player])
+    @board2 = $game.opponent_board_view $game.send(session[:player])
     erb :player1_game
   end
 
-  post '/player1_game' do
-    begin
-      coordinate = params[:coords]
-      $game.player_1.shoot coordinate.to_sym
-    rescue
+  post '/shooting' do
+      begin
+        coordinate = params[:coords]
+        @shoot_target = $game.send(session[:player]).shoot coordinate.to_sym
+      rescue
       @error = "Invalid coordinate Try Again!"
-    end
+      end
 
-    @board  = $game.own_board_view $game.player_1
-    @board2 = $game.opponent_board_view $game.player_1
-    erb :player1_game
+      @board  = $game.own_board_view $game.send(session[:player])
+      @board2 = $game.opponent_board_view $game.send(session[:player])
+
+      if !$game.has_winner?
+        erb :player1_game
+      else
+        erb :winner
+      end
+  
+  end
+
+  get '/winner' do
+    erb :winner
+  end
+
+  helpers do
+    def assign_player
+      session[:player] = PLAYERS.pop
+    end
   end
 end
